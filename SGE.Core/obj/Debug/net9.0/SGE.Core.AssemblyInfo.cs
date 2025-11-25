4e88d8a987e80e1185fb3d33a5967b3e9515b599
Enzo ANGOT <81158245+Nianoute@users.noreply.github.com>
2025-11-25 17:42:05 +0100

    feat: attendance

--- SGE.Core/obj/Debug/net9.0/SGE.Core.AssemblyInfo.cs
+++ SGE.Core/obj/Debug/net9.0/SGE.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2abb28837475848266c84883ce73a62075616df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26b5171c5f9a81126bfa52bab8922cc4dc91572")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
